2dafb0e3a7242130161fbf8ca5238d8d9e2e9586
ManishaPanda-01 <manishapanda475@gmail.com>
2025-04-20 12:47:53 +0530

    Initial commit with Jenkinsfile and app code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Node.js App CI/CD!');
+  res.end('Hello from Node.js CI/CD Pipeline! ');
 });
 
 server.listen(3000, () => {
